7097656ea88fcbf600cf20313b8e340551b6aa92
Fernando Herrera <fernando.herrera85@gmail.com>
2023-11-13 11:09:43 -0500

    Mutate => update

--- src/app/signals/pages/properties-page/properties-page.component.ts
+++ src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,43 +1,44 @@
 import { Component, OnDestroy, OnInit, computed, effect, signal } from '@angular/core';
 import { User } from '../../interfaces/user-request.interface';
 
-@Component({
+@Component( {
   templateUrl: './properties-page.component.html',
-  styleUrls: ['./properties-page.component.css']
-})
+  styleUrls: [ './properties-page.component.css' ]
+} )
 export class PropertiesPageComponent implements OnDestroy, OnInit {
 
 
-  public counter = signal(10);
+  public counter = signal( 10 );
 
-  public user = signal<User>({
+  public user = signal<User>( {
     id: 1,
     email: 'george.bluth@reqres.in',
     first_name: 'George',
     last_name: 'Bluth',
     avatar: 'https://reqres.in/img/faces/1-image.jpg'
-  });
+  } );
 
   public fullName = computed( () => `${ this.user().first_name } ${ this.user().last_name }` );
 
   public userChangedEffect = effect( () => {
-    console.log(`${ this.user().first_name} - ${ this.counter() } `);
-  });
+    // Todo: Descomentar esta línea
+    console.log( `${ this.user().first_name } - ${ this.counter() } ` );
+  } );
 
   ngOnInit(): void {
-    setInterval(() => {
+    setInterval( () => {
       this.counter.update( current => current + 1 );
 
       // if ( this.counter() == 15 )
       //   this.userChangedEffect.destroy();
-    },1000)
+    }, 1000 );
   }
 
   ngOnDestroy(): void {
     // this.userChangedEffect.destroy();
   }
 
-  increaseBy( value: number) {
+  increaseBy( value: number ) {
     this.counter.update( current => current + value );
   }
 
@@ -50,36 +51,42 @@ export class PropertiesPageComponent implements OnDestroy, OnInit {
     //   [field]: value,
     // });
 
+    // this.user.update( current => ({
+    //   ...current,
+    //   [field]: value
+    // }))
+
     this.user.update( current => {
-      return {
-        ...current,
-        [field]: value
-      };
-    });
+
+      switch ( field ) {
+
+        case 'email':
+          current.email = value;
+          break;
+
+        case 'avatar':
+          current.avatar = value;
+          break;
+
+        case 'first_name':
+          current.first_name = value;
+          break;
+
+        case 'last_name':
+          current.last_name = value;
+          break;
+
+        case 'id':
+          current.id = Number( value );
+        break;
+      }
 
 
-    // this.user.mutate( current => {
-
-    //   switch( field ) {
-    //     case 'email':
-    //       current.email = value;
-    //     break;
-
-    //     case 'first_name':
-    //       current.first_name = value;
-    //     break;
-
-    //     case 'last_name':
-    //       current.last_name = value;
-    //     break;
-
-    //     case 'id':
-    //       current.id = Number(value);
-    //     break;
-    //   }
+      return current;
+    } );
 
 
-    // });
+
 
   }
 
